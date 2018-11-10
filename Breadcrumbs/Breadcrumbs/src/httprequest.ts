a4b1754115f3919d0d5a35e507f5972300ac07c2
Tucker Myers <tucker.myers@oit.edu>
2018-11-09 17:24:45 -0800

    Added httprequests.ts (Provider for API calls) as well as introduced dynamic ion-select

---
+++ Breadcrumbs/Breadcrumbs/src/httprequest.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+/*
+  Generated class for the httprequest provider.
+
+  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
+  for more info on providers and Angular 2 DI.
+*/
+
+var aws_url = 'http://ec2-54-89-215-101.compute-1.amazonaws.com:4604'
+
+
+@Injectable()
+export class httprequest {
+
+  data: Object;
+  constructor(public http: Http) {
+    console.log('Hello httprequest Provider');
+  }
+
+  RequestContacts(userid) {
+    if (this.data) {
+      return Promise.resolve(this.data);
+    }
+    return new Promise(resolve => {
+      this.http.get(aws_url + '/contacts/'+ userid)
+        .map(res => res.json())
+        .subscribe(data => {
+          this.data = data;
+          resolve(this.data);
+        });
+    })
+  }
+
+  RequestEvents(userid) {
+    if (this.data) {
+      return Promise.resolve(this.data);
+    }
+    return new Promise(resolve => {
+      this.http.get(aws_url + '/events/' + userid)
+        .map(res => res.json())
+        .subscribe(data => {
+          this.data = data;
+          resolve(this.data);
+        });
+    })
+  }
+
+}
--- Breadcrumbs/Breadcrumbs/src/pages/addevent/addevent.ts
+++ Breadcrumbs/Breadcrumbs/src/pages/addevent/addevent.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, List } from 'ionic-angular';
 import { addcontactPage } from '../addcontact/addcontact';
-
+import { httprequest } from '../../httprequest';
+import { getContacts } from '../../api/getAllContacts.js';
 /*
   Generated class for the addevent page.
 
@@ -10,15 +11,31 @@ import { addcontactPage } from '../addcontact/addcontact';
 */
 @Component({
     selector: 'page-addevent',
-    templateUrl: 'addevent.html'
+  templateUrl: 'addevent.html',
+  providers: [httprequest]
+
 })
+
 export class addeventPage {
 
-    constructor(public navCtrl: NavController, public navParams: NavParams) { }
+
+  contacts: any;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public request: httprequest)
+  {
+    this.loadContacts(1);
+    console.log(this.contacts);
+  }
 
     ionViewDidLoad() {
-        console.log('ionViewDidLoad addeventPage');
+      console.log('ionViewDidLoad addeventPage');
     }
+
+  loadContacts(userid) {
+    this.request.RequestContacts(userid)
+      .then(data => {
+        this.contacts = data['recordset'];
+      })
+  }
 
   cancelClick() {
     this.navCtrl.pop();
