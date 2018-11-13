b4f3eb18b6b2bc84c625c98c54ba00f2171cee17
Tucker Myers <tucker.myers@oit.edu>
2018-11-12 17:01:44 -0800

    Added Current event to home page,  added add event functionality.

--- Breadcrumbs/Breadcrumbs/src/httprequest.ts
+++ Breadcrumbs/Breadcrumbs/src/httprequest.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Headers, Request, RequestOptions } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 /*
@@ -9,7 +9,7 @@ import 'rxjs/add/operator/map';
   for more info on providers and Angular 2 DI.
 */
 
-var aws_url = 'http://ec2-54-89-215-101.compute-1.amazonaws.com:4604'
+var aws_url = 'http://ec2-34-226-139-163.compute-1.amazonaws.com:4604'
 
 
 @Injectable()
@@ -48,4 +48,31 @@ export class httprequest {
     })
   }
 
+  InsertEvent(userid, eventData) {
+    var header = new Headers();
+    header.append("Accept", 'application/json');
+    header.append('Content-Type', 'application/json')
+    const requestOpts = new RequestOptions({ headers: header });
+    this.http.post(aws_url + '/newevent', eventData, requestOpts)
+      .subscribe(data => {
+        console.log(data['_body']);
+      }, error => {
+        console.log(error);
+      });
+  }
+  
+  GetActive(userid) {
+    if (this.data) {
+      return Promise.resolve(this.data);
+    }
+    return new Promise(resolve => {
+      this.http.get(aws_url + '/activeEvent/' + userid)
+        .map(res => res.json())
+        .subscribe(data => {
+          this.data = data;
+          resolve(this.data);
+        });
+    })
+  }
 }
+
--- Breadcrumbs/Breadcrumbs/src/pages/home/home.ts
+++ Breadcrumbs/Breadcrumbs/src/pages/home/home.ts
@@ -2,17 +2,29 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { addeventPage } from '../addevent/addevent';
 import { vieweventsPage } from '../viewevents/viewevents';
-
+import { httprequest } from '../../httprequest';
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers: [httprequest]
 })
 
 export class HomePage {
-  constructor(public navCtrl: NavController) {
-    
+
+  currentEvent: any;
+  constructor(public navCtrl: NavController, public request: httprequest) {
+    this.getActiveEvent();
   }
   
+  getActiveEvent() {
+    this.request.GetActive(1)
+      .then(data => {
+        console.log(data['recordset'][0])
+        this.currentEvent = data['recordset'][0];
+
+      })
+  }
+
   onLink(url: string) {
       window.open(url);
   }
