75b493fd7f71e92454290ac2d927ee4eb0957916
Samuel Bishop <35236779+sbishop88@users.noreply.github.com>
2018-11-13 14:48:52 -0800

    vieweventsPage now pulling from DB

--- Breadcrumbs/Breadcrumbs/src/pages/viewevents/viewevents.ts
+++ Breadcrumbs/Breadcrumbs/src/pages/viewevents/viewevents.ts
@@ -2,7 +2,6 @@ import { NavController, NavParams } from 'ionic-angular';
 import { Component } from '@angular/core';
 import { httprequest } from '../../httprequest';
 import { Storage } from '@ionic/storage';
-import { userid } from '../home';
 
 @Component({
     selector: 'page-viewevents',
@@ -13,21 +12,24 @@ import { userid } from '../home';
 
 export class vieweventsPage {
   activeEvent: any;
+  inactiveEvents: any;
   constructor(public navCtrl: NavController, public navParams: NavParams, public request: httprequest, public storage: Storage) {
     this.storage.get('activeEvent').then((data) => {
       //console.log(data);
       document.getElementById("activeEventButton").innerText = data.EventName;
     });
     this.storage.get('inactiveEvents').then((data) => {
+      this.inactiveEvents = data;
       //console.log('Inactive Event 0 Name: ' + data[0].EventName);
-      document.getElementById("event1").innerText = data[0].EventName;
-      document.getElementById("event2").innerText = data[1].EventName;
-      document.getElementById("event3").innerText = data[2].EventName;
-      document.getElementById("event4").innerText = data[3].EventName;
-      document.getElementById("event5").innerText = data[4].EventName;
+      //document.getElementById("event1").innerText = data[0].EventName;
+      //document.getElementById("event2").innerText = data[1].EventName;
+      //document.getElementById("event3").innerText = data[2].EventName;
+      //document.getElementById("event4").innerText = data[3].EventName;
+      //document.getElementById("event5").innerText = data[4].EventName;
     });
   }
 
   ionViewDidLoad() {
+
   }
 }
